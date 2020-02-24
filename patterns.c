6a2650a89108feeb4ebef5b8e3580bed1423e707
Kunal SIngh <49939683+singhkunal2050@users.noreply.github.com>
2020-02-24 21:43:50 +0530

    some new patterns

--- patterns.c
+++ patterns.c
@@ -1,3 +1,15 @@
+void initGOL()
+{
+	srand(time(0));                         // psuedo random base for random generation init by curr time 
+	for(i=0;i<rows;i++)
+		for(j=0;j<cols;j++)
+			if(i==j || i==rows-j && (i>50 && i<100) )										// i == hor line(col)
+				next[i][j].state=curr[i][j].state=1;									// gets the random values form 0 | 1 for even postn
+			else
+				next[i][j].state=curr[i][j].state=0;									// 0 at every odd postn
+
+}
+
 //minimal 
 
 void initGOL()
